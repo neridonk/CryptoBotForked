af4e0d1d5c86ea033341305a73ca77b74cc65ad1
neridonk <neridonk@googlemail.com>
2017-10-25 20:10:46 +0200

    Update UIconfig.js

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -13,8 +13,8 @@ const CONFIG = {
   },
   ui: {
     ssl: false,
-    host: ip,
-    port: port,
+    host: 'v22017105055054583.bestsrv.de',
+    port: '22',
     path: '/'
   },
   adapter: 'sqlite'
