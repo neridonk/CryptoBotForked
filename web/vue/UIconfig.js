6d02f064744f79393d2c4b9dfb4bd371b8865e54
domEscobar <admin@changeisamazing.com>
2018-04-08 12:52:11 +0200

    add AI

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -4,16 +4,16 @@
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
 const CONFIG = {
-  headless: true,
+  headless: false,
   api: {
     host: '127.0.0.1',
     port: 3000,
     timeout: 120000 // 2 minutes
   },
   ui: {
-    ssl: true,
-    host: 'v22017105055054583.bestsrv.de',
-    port: 443,
+    ssl: false,
+    host: 'localhost',
+    port: 3000,
     path: '/'
   },
   adapter: 'sqlite'
@@ -23,4 +23,3 @@ if (typeof window === 'undefined')
   module.exports = CONFIG;
 else
   window.CONFIG = CONFIG;
-
